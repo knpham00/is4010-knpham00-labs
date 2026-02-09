fe298c71d79831a772afb5c60837663b7dea4a6e
Kimberly Pham <phamkb@mail.uc.edu>
2026-02-08 20:58:32 -0500

    Complete Lab 04

---
+++ week04/lab04.py
@@ -0,0 +1,19 @@
+def find_common_elemens(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+
+    common = set1.intersection(set2)
+
+    return list(common) 
+
+def find_user_by_name(users, name):
+
+    user_lookup = {user['name']: user for user in users}
+    
+    return user_lookup.get(name)
+
+def get_list_of_even_numbers(numbers):
+   
+    even_numbers = [n for n in numbers if n % 2 == 0]
+    
+    return even_numbers
